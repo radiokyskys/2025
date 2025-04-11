478c6d0ead8b9fd6b5c6bbe633502dfd883910fe
Andreas Gysin <andreas@ertdfgcvb.ch>
2025-04-11 10:45:36 +0200

    Update sketch.js

--- esempi/06_poligono/sketch.js
+++ esempi/06_poligono/sketch.js
@@ -1,14 +1,18 @@
+// Crea un canvas di 600x400 pixel
 function setup(){
 	createCanvas(600, 400)
-
 }
 
 function draw() {
+	// Imposta uno sfondo grigio chiaro
 	background(220)
 
+	// Sposta l'origine al centro del canvas
 	translate(width/2, height/2)
 
+	// Imposta il raggio del poligono
 	const raggio = 100
+	// Calcola il numero di lati in base alla posizione del mouse
 	const numeroLati = Math.floor(mouseX / 10)
 
 	// noFill()
@@ -20,9 +24,11 @@ function draw() {
 	// vertex(cos(TAU / 5 * 4) * raggio, sin(TAU / 5 * 4) * raggio)
 	// endShape(CLOSE)
 
+	// Disegna un poligono regolare
 	noFill()
 	beginShape()
 	for(let i=0; i<numeroLati; i++) {
+		// Calcola l'angolo e le coordinate di ogni vertice
 		const a = TAU / numeroLati * i
 		const x = cos(a) * raggio
 		const y = sin(a) * raggio
